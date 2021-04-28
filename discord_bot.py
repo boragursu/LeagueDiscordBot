35b2c79dc67aa54e1cbb1d6c8091f9ada041cc42
boragursu <boragursu@gmail.com>
2021-04-29 00:08:53 +0300

    added new commands and error handling

--- discord_bot.py
+++ discord_bot.py
@@ -1,9 +1,8 @@
 import discord
 from discord.ext import commands
-import league
-from league import printOutPlayerData
+from league import *
 
-client = commands.Bot(command_prefix = ".")
+client = commands.Bot(command_prefix = ".", help_command=None)
 
 def read_discord_token():
     with open("tokens.txt" , "r") as f:
@@ -15,11 +14,38 @@ discord_token = read_discord_token()
 async def on_ready():
     print("Bot is running!")
 
+@client.command(pass_context = True)
+async def helplolstat(ctx):
+    author = ctx.message.author
+    embed = discord.Embed(
+        colour= discord.Colour.red()
+    )
+    embed.set_author(name= "Help")
+    embed.add_field(name = " usage lolstat {summoner name} {region code}", value = "Returns basic summoner stats", inline = False)
+
+    await ctx.send(embed=embed)
+
 @client.command()
-async def leaguestat(ctx, inputted_name):
+async def lolflexstat(ctx, inputted_name, my_region):
     print(inputted_name)
-    result = printOutPlayerData(str(inputted_name))
+    result = printOutFlexPlayerData(str(inputted_name), my_region)
     await ctx.send(result)
+
+@client.command()
+async def lolstat(ctx, inputted_name, my_region):
+    print(inputted_name)
+    result = printOutPlayerData(str(inputted_name), my_region)
+    await ctx.send(result)
+
+@lolflexstat.error
+async def lolflexstat_error(ctx, error):
+    if isinstance(error, discord.ext.commands.errors.CommandInvokeError):
+        await ctx.send("Username not found in the selected region, doesn't exist or unranked in flex queue for more information about the command write .helplolflexstat")
+
+
+@lolstat.error
+async def lolstat_error(ctx, error):
+    if isinstance(error, discord.ext.commands.errors.CommandInvokeError):
+        await ctx.send("Username not found in the selected region, doesn't exist or unranked in solo/duo queue")
+
 client.run(discord_token)
-
-
--- league.py
+++ league.py
@@ -5,17 +5,28 @@ def read_riotapi_token():
         return lines[1].strip()
 key = read_riotapi_token()
 lol_watcher = LolWatcher(key)
-def printOutPlayerData(inputted_name):
-    my_region = "tr1"
+def printOutFlexPlayerData(inputted_name,my_region):
     me = lol_watcher.summoner.by_name(my_region, inputted_name)
     ranked_stats = lol_watcher.league.by_summoner(my_region, me['id'])
 
     summoner_name = ranked_stats[0]["summonerName"]
-    rank = ranked_stats[0]["rank"]
-    tier = ranked_stats[0]["tier"]
-    leaguepoint = ranked_stats[0]["leaguePoints"]
-    wins = int(ranked_stats[0]["wins"])
-    losses = int(ranked_stats[0]["losses"])
-    winrate = round((wins/(losses+wins)) * 100)
+    flexrank = ranked_stats[0]["rank"]
+    flextier = ranked_stats[0]["tier"]
+    flexleaguepoint = ranked_stats[0]["leaguePoints"]
+    flexwins = int(ranked_stats[0]["wins"])
+    flexlosses = int(ranked_stats[0]["losses"])
+    flexwinrate = round((flexwins/(flexlosses+flexwins)) * 100)
+    return f"Hello {summoner_name} your current season flex queue rank is {flextier} {flexrank}, your current LP is {flexleaguepoint}, your current winrate is %{flexwinrate}"
 
-    return f"Hello {summoner_name} your current season rank is {tier} {rank}, your current LP is {leaguepoint}, your current winrate is %{winrate}"
+def printOutPlayerData(inputted_name,my_region):
+    me = lol_watcher.summoner.by_name(my_region, inputted_name)
+    ranked_stats = lol_watcher.league.by_summoner(my_region, me['id'])
+    summoner_name = ranked_stats[0]["summonerName"]
+    rank = ranked_stats[1]["rank"]
+    tier = ranked_stats[1]["tier"]
+    leaguepoint = ranked_stats[1]["leaguePoints"]
+    wins = int(ranked_stats[1]["wins"])
+    losses = int(ranked_stats[1]["losses"])
+    winrate = round((wins/(losses+wins)) * 100)
+    
+    return f"Hello {summoner_name} your current season solo/duo queue rank is {tier} {rank}, your current LP is {leaguepoint}, your current winrate is %{winrate}"
