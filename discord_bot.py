5da4784e2afca8fece9d12bf283a8cc0902ced91
Bora <bora@Boras-MacBook.local>
2021-04-28 19:36:50 +0300

    command added

--- discord_bot.py
+++ discord_bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import league
+from league import printOutPlayerData
 
 client = commands.Bot(command_prefix = ".")
 
@@ -13,6 +14,12 @@ discord_token = read_discord_token()
 @client.event
 async def on_ready():
     print("Bot is running!")
-    print(league.output_player)
+
+@client.command()
+async def leaguestat(ctx, inputted_name):
+    print(inputted_name)
+    result = printOutPlayerData(str(inputted_name))
+    await ctx.send(result)
 client.run(discord_token)
+
 
--- league.py
+++ league.py
@@ -5,17 +5,17 @@ def read_riotapi_token():
         return lines[1].strip()
 key = read_riotapi_token()
 lol_watcher = LolWatcher(key)
+def printOutPlayerData(inputted_name):
+    my_region = "tr1"
+    me = lol_watcher.summoner.by_name(my_region, inputted_name)
+    ranked_stats = lol_watcher.league.by_summoner(my_region, me['id'])
 
-my_region = "tr1"
-me = lol_watcher.summoner.by_name(my_region, "Dalyn")
-ranked_stats = lol_watcher.league.by_summoner(my_region, me['id'])
+    summoner_name = ranked_stats[0]["summonerName"]
+    rank = ranked_stats[0]["rank"]
+    tier = ranked_stats[0]["tier"]
+    leaguepoint = ranked_stats[0]["leaguePoints"]
+    wins = int(ranked_stats[0]["wins"])
+    losses = int(ranked_stats[0]["losses"])
+    winrate = round((wins/(losses+wins)) * 100)
 
-summoner_name = ranked_stats[0]["summonerName"]
-rank = ranked_stats[0]["rank"]
-tier = ranked_stats[0]["tier"]
-leaguepoint = ranked_stats[0]["leaguePoints"]
-wins = int(ranked_stats[0]["wins"])
-losses = int(ranked_stats[0]["losses"])
-winrate = round((wins/(losses+wins)) * 100)
-
-output_player = f"Hello {summoner_name} your current season rank is {tier} {rank}, your current LP is {leaguepoint}, your current winrate is %{winrate}"
+    return f"Hello {summoner_name} your current season rank is {tier} {rank}, your current LP is {leaguepoint}, your current winrate is %{winrate}"
